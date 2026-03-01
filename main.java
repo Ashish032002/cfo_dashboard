10331ad3205acce8840c9e2a9f45043db2724eb6
Ashish Singh <90508026+Ashish032002@users.noreply.github.com>
2026-03-01 13:24:00 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,64 @@
-package com.cams.core.rulesui;
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 
+         https://maven.apache.org/xsd/maven-4.0.0.xsd">
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+    <modelVersion>4.0.0</modelVersion>
 
-@SpringBootApplication
-public class RulesUiBackendApplication {
+    <groupId>com.cams</groupId>
+    <artifactId>rules-ui-client-backend</artifactId>
+    <version>0.0.1-SNAPSHOT</version>
+    <name>rules-ui-client-backend</name>
+    <description>Rules UI Backend - Fetch DRL from GitLab</description>
 
-    public static void main(String[] args) {
-        SpringApplication.run(RulesUiBackendApplication.class, args);
-    }
-}
+    <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>3.3.2</version>
+        <relativePath/>
+    </parent>
+
+    <properties>
+        <java.version>17</java.version>
+    </properties>
+
+    <dependencies>
+
+        <!-- REST API -->
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-web</artifactId>
+        </dependency>
+
+        <!-- WebClient -->
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-webflux</artifactId>
+        </dependency>
+
+        <!-- Config processor -->
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-configuration-processor</artifactId>
+            <optional>true</optional>
+        </dependency>
+
+        <!-- Test -->
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-test</artifactId>
+            <scope>test</scope>
+        </dependency>
+
+    </dependencies>
+
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+        </plugins>
+    </build>
+
+</project>
